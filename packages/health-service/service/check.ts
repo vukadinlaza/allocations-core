1d999291d0ce25848e15a5b53fe3caef982b2e84
Chase Abbott <cabbott93@gmail.com>
2022-12-05 13:19:23 -0800

    add health service

---
+++ packages/health-service/service/check.ts
@@ -0,0 +1,19 @@
+import { SNSEvent } from "aws-lambda";
+
+export const handler = async ({ Records }: SNSEvent): Promise<void> => {
+  for (const record of Records) {
+    try {
+      const alarm = JSON.parse(record.Sns.Message);
+      const { AlarmName } = alarm;
+      await fetch("https://hooks.zapier.com/hooks/catch/10079476/bnfsy63/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ AlarmName }),
+      });
+    } catch (e: any) {
+      console.error(e);
+    }
+  }
+};
